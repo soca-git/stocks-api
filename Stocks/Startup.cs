1d7ea5c445c674411287c6cf7a4673b0feceb9a9
Seán O'Callaghan <soca.mailbox@gmail.com>
2022-09-05 21:03:18 +0100

    update startup

--- Stocks/Startup.cs
+++ Stocks/Startup.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Stocks.Controllers.Reference.WeatherForecast;
 
 namespace Stocks
