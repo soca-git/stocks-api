7fb2a07d0323998fa19625463c7d745a8ccc0296
soca-git <soca.mailbox@gmail.com>
2022-10-05 19:13:06 +0100

    add ticker symbols controller

---
+++ Stocks.Api/Reference/Data/ITickerSymbols.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Stocks.Api.Reference.Data
+{
+    public interface ITickerSymbols
+    {
+        /// <summary>
+        /// Returns a list of all ticker symbols on the platform.
+        /// </summary>
+        /// <remarks>
+        /// Add more description here!
+        /// </remarks>
+        List<string> Get();
+    }
+}
---
+++ Stocks.Controllers/Reference/Data/TickerSymbolsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stocks.Api.Reference.Data;
+using Stocks.Cache;
+using Stocks.Controllers.Uri;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stocks.Controllers.Reference.Data
+{
+    [ApiController]
+    [Route(BaseUri.GatewayPrefix + "/reference/data/tickersymbols")]
+    public class TickerSymbolsController : ControllerBase, ITickerSymbols
+    {
+        /// <inheritdoc/>
+        public List<string> Get()
+        {
+            return DataCache.StockBasicInformation.Keys.ToList();
+        }
+    }
+}
