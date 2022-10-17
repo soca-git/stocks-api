2e28a8fedfc8db1330c5dadd9d45c2076e6f055b
soca-git <soca.mailbox@gmail.com>
2022-10-17 13:03:25 +0100

    add some basic unit tests

---
+++ Stocks.Tests/Stocks.Shared/Utils/EnumUtilsTests.cs
@@ -0,0 +1,19 @@
+﻿using Stocks.Shared.Utils;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.Shared.Utils
+{
+    public class NamespaceUtilsTests
+    {
+        [Fact]
+        public void GetNamespaceSections_Returns_Array_Of_Namespace_Section_Strings()
+        {
+            var mockNamespace = "Hello.World";
+            var namespaceSections = mockNamespace.GetNamespaceSections();
+
+            Assert.IsType<string[]>(namespaceSections);
+            Assert.NotEmpty(namespaceSections);
+            Assert.Equal(new string[] { "Hello", "World"}, namespaceSections);
+        }
+    }
+}
---
+++ Stocks.Tests/Stocks.Shared/Utils/NamespaceUtilsTests.cs
@@ -0,0 +1,53 @@
+﻿using Stocks.Shared.Utils;
+using System;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.Shared.Utils
+{
+    public class EnumUtilsTests
+    {
+        private readonly string _validEnumStringValue = "EUR";
+        private readonly string _invalidEnumStringValue = "EURO";
+        private readonly MockEnum _validEnumValue = MockEnum.EUR;
+        private readonly MockEnum _defaultEnumValue = MockEnum.USD;
+
+        private enum MockEnum
+        {
+            EUR,
+            USD
+        }
+
+        [Fact]
+        public void ToEnumOrDefault_Returns_Enum_When_Value_Matches()
+        {
+            var enumValue = _validEnumStringValue.ToEnumOrDefault(_defaultEnumValue);
+
+            Assert.IsType<MockEnum>(enumValue);
+            Assert.Equal(_validEnumValue, enumValue);
+        }
+
+        [Fact]
+        public void ToEnumOrDefault_Returns_Default_Enum_When_Value_Does_Not_Match()
+        {
+            var enumValue = _invalidEnumStringValue.ToEnumOrDefault(_defaultEnumValue);
+
+            Assert.IsType<MockEnum>(enumValue);
+            Assert.Equal(_defaultEnumValue, enumValue);
+        }
+
+        [Fact]
+        public void ToEnum_Returns_Enum_When_Value_Matches()
+        {
+            var enumValue = _validEnumStringValue.ToEnum<MockEnum>();
+
+            Assert.IsType<MockEnum>(enumValue);
+            Assert.Equal(_validEnumValue, enumValue);
+        }
+
+        [Fact]
+        public void ToEnum_Throws_Exception_When_Value_Does_Not_Match()
+        {
+            Assert.Throws<Exception>(() => _invalidEnumStringValue.ToEnum<MockEnum>());
+        }
+    }
+}
