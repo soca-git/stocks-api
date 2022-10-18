458d4d361d00ee23a5ff1901689c3eb8229813ec
soca-git <soca.mailbox@gmail.com>
2022-10-18 18:46:51 +0100

    add comments

--- src/Stocks/Bootstrap/MvcBuilderExtensions.cs
+++ src/Stocks/Bootstrap/MvcBuilderExtensions.cs
@@ -10,7 +10,14 @@ namespace Stocks.Bootstrap
         {
             builder.AddJsonOptions(options =>
             {
+                // This means response property names will be serialized as PascalCase (instead of camelCase by default).
+                // Aligns with generated OpenAPI TypeScript clients.
+                // The original discrepancy was that the OpenAPI specification is generated with PascalCase property names from the API interface contracts,
+                // while the API was returning camelCase property names in the serialized response.
                 options.JsonSerializerOptions.PropertyNamingPolicy = null;
+
+                // This means response enum values will be serialized to their string representation (instead of integer by default).
+                // The same converter is added to NSwag to keep the OpenAPI specification consistent.
                 options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
             });
 
