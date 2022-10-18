e969ec02b82b5e1fd24ceb6c98f9680ec81ee50c
Seán O'Callaghan <60453614+soca-git@users.noreply.github.com>
2022-10-19 00:11:34 +0100

    Update MarketInformationMapper.cs
    update access modifier

--- src/Stocks.Controllers/_Internal/Mappers/MarketInformationMapper.cs
+++ src/Stocks.Controllers/_Internal/Mappers/MarketInformationMapper.cs
@@ -1,11 +1,11 @@
-﻿using IEXSharp.Model.CoreData.ReferenceData.Response;
+using IEXSharp.Model.CoreData.ReferenceData.Response;
 using Stocks.Api.Common.Contracts;
 using Stocks.Shared.Extensions;
 using System.Collections.Generic;
 
 namespace Stocks.Controllers._Internal.Mappers
 {
-    public static class MarketInformationMapper
+    internal static class MarketInformationMapper
     {
         public static Dictionary<string, MarketInformation> ToMarketInformationHash(this IEnumerable<ExchangeInternationalResponse> source)
         {
