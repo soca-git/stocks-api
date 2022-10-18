93d7d6a900d1898d820fff9f991c90611b5a9b5f
soca-git <soca.mailbox@gmail.com>
2022-10-18 22:25:24 +0100

    implement IMarketStatus API

---
+++ src/Stocks.Controllers/Status/Markets/MarketStatusController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stocks.Api.Status.Market;
+using Stocks.Api.Status.Market.Contracts;
+using Stocks.Cache;
+using Stocks.Controllers._Internal.IEXCloud;
+using Stocks.Controllers._Internal.Utils;
+using Stocks.Controllers.Uri;
+using System.Threading.Tasks;
+
+namespace Stocks.Controllers.Status.Markets
+{
+    /// <inheritdoc/>
+    [ApiController]
+    [Route(BaseUri.GatewayPrefix + "/status/markets")]
+    public class MarketStatusController : ControllerBase, IMarketStatus
+    {
+        private readonly IEXClient client = new IEXClient();
+        private IDataCache _cache;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="cache"></param>
+        public MarketStatusController(IDataCache cache)
+        {
+            _cache = cache;
+        }
+
+        /// <inheritdoc/>
+        public async Task<MarketStatusPreview> Get([FromQuery] MarketStatusQuery query)
+        {
+            var quote = await client.Api.StockPrices.QuoteAsync(query.TickerSymbol);
+            var response = new MarketStatusPreview();
+
+            if (quote.Data != null)
+            {
+                var market = _cache.StockInformation[query.TickerSymbol].Market;
+                response.Name = _cache.MarketInformation[market].FullName;
+                response.Status = MarketStatusUtils.CalculateMarketStatus(quote.Data.iexOpenTime.Value, quote.Data.iexCloseTime.Value);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ src/Stocks.Controllers/_Internal/Utils/MarketStatusUtils.cs
@@ -0,0 +1,26 @@
+﻿using Stocks.Api.Status.Market.Contracts;
+using System;
+
+namespace Stocks.Controllers._Internal.Utils
+{
+    internal static class MarketStatusUtils
+    {
+        public static MarketStatus CalculateMarketStatus(decimal openTime, decimal closeTime)
+        {
+            var current = MillisecondsSinceEpochNow();
+
+            if (current > openTime && current < closeTime)
+            {
+                return MarketStatus.Open;
+            }
+
+            return MarketStatus.Closed;
+        }
+
+        private static decimal MillisecondsSinceEpochNow()
+        {
+            var timeSpanSinceEpoch = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return (decimal)timeSpanSinceEpoch.TotalMilliseconds;
+        }
+    }
+}
