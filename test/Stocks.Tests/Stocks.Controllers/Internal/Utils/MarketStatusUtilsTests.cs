c435c86ab1dc1e828752e4932545cdeab626b17c
soca-git <soca.mailbox@gmail.com>
2022-10-20 11:20:07 +0100

    add market status calculator tests

---
+++ test/Stocks.Tests/Stocks.Controllers/Internal/Utils/MarketStatusUtilsTests.cs
@@ -0,0 +1,29 @@
+﻿using Stocks.Api.Status.Market.Contracts;
+using Stocks.Controllers._Internal.Utils;
+using Stocks.Shared.Utils;
+using System;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.Controllers.Internal.Utils
+{
+    public class MarketStatusUtilsTests
+    {
+        [Fact]
+        public void Calculate_Market_Status_When_Market_Is_Open()
+        {
+            var openTime = DateTimeUtils.MillisecondsSinceEpoch(DateTime.UtcNow.AddHours(-1));
+            var closeTime = DateTimeUtils.MillisecondsSinceEpoch(DateTime.UtcNow.AddHours(+1));
+
+            Assert.Equal(MarketStatus.Open, MarketStatusUtils.CalculateMarketStatus(openTime, closeTime));
+        }
+
+        [Fact]
+        public void Calculate_Market_Status_When_Market_Is_Closed()
+        {
+            var openTime = DateTimeUtils.MillisecondsSinceEpoch(DateTime.UtcNow.AddHours(-13));
+            var closeTime = DateTimeUtils.MillisecondsSinceEpoch(DateTime.UtcNow.AddHours(-1));
+
+            Assert.Equal(MarketStatus.Closed, MarketStatusUtils.CalculateMarketStatus(openTime, closeTime));
+        }
+    }
+}
