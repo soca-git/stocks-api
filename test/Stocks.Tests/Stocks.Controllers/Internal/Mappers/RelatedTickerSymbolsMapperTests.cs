cf710cd47773b0cfae433ff6be5370791bb2074f
soca-git <soca.mailbox@gmail.com>
2022-10-30 13:56:51 +0000

    add mapper tests

---
+++ test/Stocks.Tests/Stocks.Controllers/Internal/Mappers/RelatedTickerSymbolsMapperTests.cs
@@ -0,0 +1,27 @@
+﻿using Stocks.Controllers._Internal.Mappers;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.Controllers.Internal.Mappers
+{
+    public class RelatedTickerSymbolsMapperTests
+    {
+        [Fact]
+        public void Related_Ticker_Symbols_Mapper_Works_When_Multiple_Symbols()
+        {
+            Assert.Equal(new List<string> { "QCOM", "AAPL" }, "QCOM,AAPL".ToRelatedTickerSymbols());
+        }
+
+        [Fact]
+        public void Related_Ticker_Symbols_Mapper_Works_When_Single_Symbol()
+        {
+            Assert.Equal(new List<string> { "QCOM" }, "QCOM".ToRelatedTickerSymbols());
+        }
+
+        [Fact]
+        public void Related_Ticker_Symbols_Mapper_Works_When_Empty_String()
+        {
+            Assert.Equal(new List<string> { "" }, "".ToRelatedTickerSymbols());
+        }
+    }
+}
