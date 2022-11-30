316c952cd1213ec5a6207cf86ea3b43a1bf64ab4
soca-git <soca.mailbox@gmail.com>
2022-11-30 13:11:02 +0000

    hide sensitive keys in environment variables

--- src/Stocks.IEXCloud/IEXClient.cs
+++ src/Stocks.IEXCloud/IEXClient.cs
@@ -1,19 +1,18 @@
 ﻿using IEXSharp;
+using System;
 
 namespace Stocks.IEXCloud
 {
     internal class IEXClient : IIEXClient
     {
-        private const string PublicToken = "pk_d7996497b8844a999541274497663935";
-        private const string SecretToken = "sk_5b4f8a0d878245189977ce9a2d366c50";
-        //private const string PublicToken = "pk_593820d13f6a47f8a429bcd0be14b0f9";
-        //private const string SecretToken = "sk_0b01190d64bf4bb1945ec45aefe10173";
-
         private readonly IEXCloudClient _client;
 
         public IEXClient()
         {
-            _client = new IEXCloudClient(PublicToken, SecretToken, signRequest: false, useSandBox: false);
+            string sk = Environment.GetEnvironmentVariable("IEX_SK");
+            string pk = Environment.GetEnvironmentVariable("IEX_PK");
+
+            _client = new IEXCloudClient(pk, sk, signRequest: false, useSandBox: false);
         }
 
         public IEXCloudClient Api => _client;
