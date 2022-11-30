7ddd409cbb58a5391c5cda3918deca63aa9cc305
soca-git <soca.mailbox@gmail.com>
2022-11-30 11:50:44 +0000

    add ping endpoint

---
+++ src/Stocks.Api/Infrastructure/Ping/IPing.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Stocks.Api.Infrastructure.Ping
+{
+    public interface IPing
+    {
+        /// <summary>
+        /// Returns a friendly message if the server is accessible.
+        /// </summary>
+        Task<string> Get();
+    }
+}
---
+++ src/Stocks.Controllers/Infrastructure/Ping/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stocks.Api.Infrastructure.Ping;
+using Stocks.Controllers.Uri;
+using System.Threading.Tasks;
+
+namespace Stocks.Controllers.Infrastructure.Ping
+{
+    /// <summary>
+    /// </summary>
+    [ApiController]
+    [Route(BaseUri.GatewayPrefix + "/ping")]
+    public class PingController : ControllerBase, IPing
+    {
+        /// <inheritdoc/>
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("Hello from Stocks-API!");
+        }
+    }
+}
