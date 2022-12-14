c40f5af26edd62d0ff2d17c452b6d1b9bfc8ce00
soca-git <soca.mailbox@gmail.com>
2022-12-14 13:19:21 +0000

    add some response sample tests

---
+++ test/Stocks.Tests/Stocks.NSwag/Bootstrap/Context/AddNSwagContextTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using Stocks.NSwag.Bootstrap.Context;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.NSwag.Bootstrap.Context
+{
+    public class AddNSwagContextTests
+    {
+        private readonly Mock<IConfiguration> _configuration = new Mock<IConfiguration>();
+        private readonly Assembly _someAssembly = typeof(AddNSwagContextTests).Assembly;
+
+        [Fact]
+        public void Successfully_Create_Context()
+        {
+            new AddNSwagContext(setup => 
+            {
+                setup.Configuration = _configuration.Object;
+                setup.ControllerAssembly = _someAssembly;
+                setup.DocumentationAssembly = _someAssembly;
+            });
+        }
+
+        [Fact]
+        public void Create_Context_With_Missing_Setup()
+        {
+            Action<AddNSwagContext> setupAction = setup => 
+            {
+                setup.Configuration = _configuration.Object;
+                setup.ControllerAssembly = _someAssembly;
+            };
+
+            Assert.Throws<Exception>(() => new AddNSwagContext(setupAction));
+        }
+    }
+}
---
+++ test/Stocks.Tests/Stocks.NSwag/Registers/ResponseSamples/ResponseSampleRegisterTests.cs
@@ -0,0 +1,30 @@
+﻿using Stocks.NSwag.Registers.ResponseSamples;
+using Xunit;
+
+namespace Stocks.Tests.Stocks.NSwag.Registers.ResponseSamples
+{
+    public class ResponseSampleRegisterTests
+    {
+        private readonly ResponseSampleRegister _register;
+
+        public ResponseSampleRegisterTests()
+        {
+            _register = new ResponseSampleRegister();
+        }
+
+        [Fact]
+        public void Register_And_Retrieve_Response_Sample_Test()
+        {
+            var method = _register.GetType().GetMethod("AddResponseSample");
+            var contract = new TestContract();
+
+            _register.AddResponseSample(method, contract, "Test Response Sample");
+
+            Assert.NotEmpty(_register.GetResponseSamples(method));
+        }
+
+        private class TestContract
+        {
+        }
+    }
+}
